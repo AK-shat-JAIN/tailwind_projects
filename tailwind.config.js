ca57090477d44c412f623d44684659e607940804
Akshat Jain <84455686+AK-shat-JAIN@users.noreply.github.com>
2023-09-25 04:31:47 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],         //path to your html file.
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
